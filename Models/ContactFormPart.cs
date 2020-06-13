e6b20e5b2d640349a73fdee7b68f3c8a5b7fba74
neuroanas <anas.hajeechiming@gmail.com>
2020-06-13 15:07:35 +0700

    Edit layout and add style for UI contact us

--- Models/ContactFormPart.cs
+++ Models/ContactFormPart.cs
@@ -1,10 +1,18 @@
-﻿using Orchard.ContentManagement;
+﻿using System.ComponentModel;
+using Orchard.ContentManagement;
 
 namespace Codesanook.OrganizationProfile.Models {
     public class ContactFormPart: ContentPart {
+        [DisplayName("Name")]
         public string Name { get; set; }
+
+        [DisplayName("Email Address")]
         public string EmailAddress { get; set; }
+
+        [DisplayName("Phone Number")]
         public string MobilePhoneNumber { get; set; }
+
+        [DisplayName("Message")]
         public string Message { get; set; }
     }
 }

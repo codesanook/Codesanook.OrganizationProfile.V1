b2ae5da94f3d7a41bf1634b31daf217fe99fa075
aaronamm <theeranitp@gmail.com>
2020-06-17 17:28:30 +0700

    Add default MVC error message style

--- Models/ContactFormPart.cs
+++ Models/ContactFormPart.cs
@@ -1,16 +1,22 @@
 ﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using Orchard.ContentManagement;
 
 namespace Codesanook.OrganizationProfile.Models {
     public class ContactFormPart: ContentPart {
+
+        [Required]
         public string Name { get; set; }
 
         [DisplayName("Email Address")]
+        [Required]
         public string EmailAddress { get; set; }
 
         [DisplayName("Phone Number")]
+        [Required]
         public string MobilePhoneNumber { get; set; }
 
+        [Required]
         public string Message { get; set; }
     }
 }
